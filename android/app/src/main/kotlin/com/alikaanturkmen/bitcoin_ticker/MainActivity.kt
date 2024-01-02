810ea5cc2a3680e74a984ea1341d0b82739d244f
Ali Kaan TURKMEN <turkmenalikaan@gmail.com>
2024-01-02 18:12:18 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/alikaanturkmen/bitcoin_ticker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alikaanturkmen.bitcoin_ticker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/coin_data.dart
@@ -0,0 +1,53 @@
+import 'networking.dart';
+import 'dart:convert';
+
+const coinApiUrl = 'https://rest.coinapi.io/v1/exchangerate';
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+class CoinData {
+  final String crypto;
+  final String currency;
+
+  CoinData(this.crypto, this.currency);
+
+  void getCoinData()async {
+    var url = '$coinApiUrl/$crypto/$currency/apikey-$apiKey';
+    print(url);
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var data = await networkHelper.getData();
+    print(data);
+
+    var lastPrice = data['rate'];
+    print(lastPrice);
+  }
+}
